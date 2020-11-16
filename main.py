010c68f78ddda6535076ecffc5e113c60fddcda1
Julia Huzvar <jhuzvar24@biomedscienceacademy.org>
2020-11-16 23:58:15 +0000

    I written the story and added the inputs. After adding the inputs into the code a paragraph form.

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ Period/Core:
 
 
 """
+
+name = input("enter your name: ")
+number = input("enter a number: ")
+place1 = input("enter a place: ")
+year = input("enter a year: ")
+animal1 = input("enter any animal: ")
+animal2 = input("name the animal: ")
+verb1 = input("enter a verb: ")
+place2 = input("enter a place: ")
+noun1 = input("enter a noun: ")
+noun2 = input("enter a noun: ")
+adverb = input("enter an adverb: ")
+adjective1 = input("enter a adjective: ")
+adjective2 = input("enter a adjective: ")
+noun4 = input("enter a noun: ")
+adjective3 = input("enter a adjective: ")
+
+print(f"My name is {name} and I've known you for {number} year(s). \nWe first met at {place1} all the way back in {year}, and I knew that you and you're {animal1} were a fun bunch. I didn't know that {animal2} was going to {verb1} so soon. Remember that time when we were at {place2} and that {noun1} jumped the {noun2} without even {adverb}. That was crazy! Well I hope you have a {adjective1} life with your {adjective2} {noun4}, and I hope to {adjective3} see you again.")
